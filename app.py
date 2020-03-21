a6f26c91b02442461eea75dda49cfe904d9fc2b2
wei-ting-yap <yap_weiting@hotmail.com>
2020-03-22 03:46:12 +0800

    header, add task, 3 columns

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) #instantiate Flask app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/weiting/School/19-20/cs162/kanban/kanban.db'
+
+db = SQLAlchemy(app) #instantiate SQLAlchemy
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    status = db.Column(db.String, default='todo')
+
+@app.route('/')
+def index():
+    todo = Tasks.query.filter_by(status='todo').all()
+    return render_template('index.html', todo=todo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
